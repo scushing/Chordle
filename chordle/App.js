b9ef28fcd6125d2b04994085920c4dea4f7b3156
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-12-13 15:31:09 -0800

    Structural Changes and Removals

--- chordle/App.js
+++ chordle/App.js
@@ -4,7 +4,8 @@ import { Keyboard, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <p>Here's something</p>
+      <Word/>
       <StatusBar style="auto" />
       <Keyboard/>
     </View>
